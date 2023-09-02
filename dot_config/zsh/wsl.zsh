5bd5903bc98ae78a5cc2b293b47f2c9d29d01bb0
Dylan Meunier <dylan19170@gmail.com>
2023-09-02 09:52:18 +0200

    added wsl keepassxc windows access from wsl, no passwordprompt now

--- dot_config/zsh/wsl.zsh
+++ dot_config/zsh/wsl.zsh
@@ -5,17 +5,19 @@ if [ -f /etc/wsl.conf ]; then
 # Start ssh-agent
 if [ -z "$SSH_AUTH_SOCK" ]; then
   eval "$(ssh-agent -s)"
-  ssh-add /home/dylan/.ssh/github_rsa
 fi
 
-# Start gpg-agent
-if [ -z "$GPG_AGENT_INFO" ]; then
-  eval "$(gpg-agent --daemon)"
-fi
 #Mount network drive 
-
 if [ -f ~/networkdrive.sh ]; then
     ~/networkdrive.sh
+fi
+
+# keepass windows key authen without password
+export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
+ss -a | grep -q $SSH_AUTH_SOCK
+if [ $? -ne 0 ]; then
+    rm -f $SSH_AUTH_SOCK
+    (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$HOME/npiperelay/npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
 fi
 
 fi
