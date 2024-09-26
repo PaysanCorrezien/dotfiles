42afdb0f7d31c218d6308df0659e6f3d876e3704
Dylan Meunier <dylan19170@gmail.com>
2024-09-26 17:16:29 +0200

    comment some parts

--- dot_config/zsh/wsl.zsh
+++ dot_config/zsh/wsl.zsh
@@ -3,9 +3,9 @@ if [ -f /etc/wsl.conf ]; then
   # echo "This is WSL"
 
 # Start ssh-agent
-if [ -z "$SSH_AUTH_SOCK" ]; then
-  eval "$(ssh-agent -s >/dev/null 2>&1)"
-fi
+# if [ -z "$SSH_AUTH_SOCK" ]; then
+#   eval "$(ssh-agent -s >/dev/null 2>&1)"
+# fi
 
 #Mount network drive 
 if [ -f ~/networkdrive.sh ]; then
@@ -22,16 +22,15 @@ fi
 
 #NOTE: SSH ON WSL with keepassxc
 # Path to npiperelay executable
-export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
-NPIPERELAY_PATH="$HOME/npiperelay/npiperelay.exe"
-# Check if npiperelay and socat are available
-if [[ -f "$NPIPERELAY_PATH" ]] && command -v socat > /dev/null; then
-    rm -f /home/dylan/.ssh/agent.sock 
-    # Run the command to set up the SSH agent
-    # (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$NPIPERELAY_PATH -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
-    # Debug version with log
-    (setsid socat -d -d UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$HOME/npiperelay/npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &) >/tmp/socat.log 2>&1
-fi
-
+# export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
+# NPIPERELAY_PATH="$HOME/npiperelay/npiperelay.exe"
+# # Check if npiperelay and socat are available
+# if [[ -f "$NPIPERELAY_PATH" ]] && command -v socat > /dev/null; then
+#     rm -f /home/dylan/.ssh/agent.sock 
+#     # Run the command to set up the SSH agent
+#     # (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$NPIPERELAY_PATH -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
+#     # Debug version with log
+#     (setsid socat -d -d UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$HOME/npiperelay/npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &) >/tmp/socat.log 2>&1
+# fi
 
 fi
